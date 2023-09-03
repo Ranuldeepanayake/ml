23dc1895e43301cc47c900c00abf22b91a6106c2
Ranul Deepanayake <ranuldeepanayake@outlook.com>
2023-09-03 14:00:59 +0530

    Updates

--- Sales-Prediction-master/2048035_Lab3-python.py
+++ Sales-Prediction-master/2048035_Lab3-python.py
@@ -6,14 +6,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-#Reading the dataset
+#Reading the dataset from the file.
 dataset = pd.read_csv("advertising.csv")
 
+## Data preprocessing
 dataset.head()
+
+# Check the structure of the data set (rows and columns).
 dataset.shape
+
+# Check for missing values.
 dataset.isna().sum()
+
+# Check for duplicate values.
 dataset.duplicated().any()
 
+## End of data preprocessing
+
+# Checking for outliers in the data set.
 fig, axs = plt.subplots(3, figsize = (5,5))
 plt1 = sns.boxplot(dataset['TV'], ax = axs[0])
 plt2 = sns.boxplot(dataset['Newspaper'], ax = axs[1])
@@ -21,8 +31,11 @@ plt3 = sns.boxplot(dataset['Radio'], ax = axs[2])
 plt.tight_layout()
 
 sns.distplot(dataset['Sales']);
+
+# Show the relationships between sales and modes of advertising.
 sns.pairplot(dataset, x_vars=['TV', 'Radio', 'Newspaper'], y_vars='Sales', height=4, aspect=1, kind='scatter')
 plt.show()
+
 sns.heatmap(dataset.corr(), annot = True)
 plt.show()
 
@@ -31,7 +44,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn import metrics
 
-## Simple linear regression
+## Simple linear regression (model #1).
 
 #Setting the value for X and Y
 x = dataset[['TV']]
@@ -45,9 +58,9 @@ slr.fit(x_train, y_train)
 #Printing the model coefficients
 print('Intercept: ', slr.intercept_)
 print('Coefficient:', slr.coef_)
-
 print('Regression Equation: Sales = 6.948 + 0.054 * TV')
-#Line of best fit
+
+#Show the line of best fit
 plt.scatter(x_train, y_train)
 plt.plot(x_train, 6.948 + 0.054*x_train, 'r')
 plt.show()
@@ -77,7 +90,7 @@ print('Mean Absolute Error:', meanAbErr)
 print('Mean Square Error:', meanSqErr)
 print('Root Mean Square Error:', rootMeanSqErr)
 
-# Multiple linear regression.
+# Multiple linear regression (model #2).
 
 #Setting the value for X and Y
 x = dataset[['TV', 'Radio', 'Newspaper']]
@@ -96,3 +109,23 @@ list(zip(x, mlr.coef_))
 #Predicting the Test and Train set result 
 y_pred_mlr= mlr.predict(x_test)  
 x_pred_mlr= mlr.predict(x_train) 
+print("Prediction for test set: {}".format(y_pred_mlr))
+
+#Actual value and the predicted value
+mlr_diff = pd.DataFrame({'Actual value': y_test, 'Predicted value': y_pred_mlr})
+mlr_diff
+
+#Predict for any value
+mlr.predict([[56, 55, 67]])
+
+# print the R-squared value for the model
+print('R squared value of the model: {:.2f}'.format(mlr.score(x,y)*100))
+
+# 0 means the model is perfect. Therefore the value should be as close to 0 as possible
+meanAbErr = metrics.mean_absolute_error(y_test, y_pred_mlr)
+meanSqErr = metrics.mean_squared_error(y_test, y_pred_mlr)
+rootMeanSqErr = np.sqrt(metrics.mean_squared_error(y_test, y_pred_mlr))
+
+print('Mean Absolute Error:', meanAbErr)
+print('Mean Square Error:', meanSqErr)
+print('Root Mean Square Error:', rootMeanSqErr)
